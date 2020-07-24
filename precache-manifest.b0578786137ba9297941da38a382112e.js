cebb32b13543742ca4f43fceba9d658acfcaa5f7
Sushant Kumar <sushantk.2897@gmail.com>
2020-07-24 19:16:38 +0530

    Updates

---
+++ precache-manifest.b0578786137ba9297941da38a382112e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48bfd367462424af2f1de7abd9f26f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "554077e7e172419a233e",
+    "url": "/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "05b99a9299fb681bf701",
+    "url": "/static/css/main.ea235ec5.chunk.css"
+  },
+  {
+    "revision": "554077e7e172419a233e",
+    "url": "/static/js/2.2da7a2b9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2da7a2b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b99a9299fb681bf701",
+    "url": "/static/js/main.b77a124c.chunk.js"
+  },
+  {
+    "revision": "ac03324763920a9097a5",
+    "url": "/static/js/runtime-main.19b1f516.js"
+  }
+]);
